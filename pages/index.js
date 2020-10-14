58e32359e6b60578a262b1391fb25c400f32c442
Shaun Reddiar <shaun.reddiar@dimensiondata.com>
2020-10-14 14:58:33 +0200

    Added toolbar to top evelcomponent

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Layout from "../components/layout.js";
 import fetch from "isomorphic-fetch";
 import Error from "next/error";
 import ProfileList from "../components/profileList";
-import ProfileCard from "../components/profileCard";
 
 class Index extends React.Component {
     static async getInitialProps() {
@@ -19,18 +18,58 @@ class Index extends React.Component {
         return { profiles };
     }
 
-    state = this.props;
+    searchProfiles(searchTerm) {
+        console.log(searchTerm);
+    }
 
     render() {
 
-        if(this.state.profiles.length === 0) {
+        if(this.props.profiles.length === 0) {
             return <Error statusCode={503} />
         }
 
         return (
-            <Layout title="Profiles">
-                <ProfileList profilesArray={ this.state.profiles } />
-            </Layout>
+            <div>
+                <Layout title="Profiles">
+                    <div className="container">
+                        <div className="profileList-toolbar">
+                            <div className="profileList-toolbar-search">
+                                <input onInput={(e)=> this.searchProfiles(e.target.value)} type="text" placeholder="Search" />
+                            </div>
+                        <div className="flex-spacer"></div>
+                        <div className="profileList-toolbar-buttons">
+                            <button>Filter by</button>
+                            <button>Sort by</button>
+                            <button>Reset</button>
+                        </div>
+                        </div>
+                    </div>
+                    <ProfileList profilesArray={ this.props.profiles } />
+                </Layout>
+                <style jsx>{`
+                    .flex-spacer {
+                        flex-grow: 1;
+                    }
+
+                    .profileList-toolbar {
+                        flex-basis:100%;
+                        display: flex;
+                        flex-direction: row;
+                        background-color: var(--accent-color);
+                        color: #fff;
+                        padding: 10px;
+                        margin: 20px 0;
+                    }
+
+                    .profileList-toolbar-search {
+                        flex-basis: 50%;
+                    }
+
+                    .profileList-toolbar-search input {
+                        width: 100%;
+                    }
+                `}</style>
+            </div>
         )
     }
 };
